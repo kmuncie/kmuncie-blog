a2649356c59e97335265a780f44ee1930f41d572
Kevin Muncie <kevin@kmuncie.com>
2020-08-01 17:08:27 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 hexo generate
-aws s3 sync public s3://kmuncie.com/blog/ --delete
-aws cloudfront create-invalidation --distribution-id E2YSOO8G0GOROD --paths "/blog/*"
-aws cloudfront list-invalidations --distribution-id E2YSOO8G0GOROD
+aws s3 sync public s3://kmuncie-site-prd/blog/ --delete --profile kmuncie
+aws cloudfront create-invalidation --distribution-id ENQKZZIYNE7H4 --paths "/blog/*" --profile kmuncie
+aws cloudfront list-invalidations --distribution-id ENQKZZIYNE7H4 --profile kmuncie
 
